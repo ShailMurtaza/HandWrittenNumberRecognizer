7dedab5dd53450e3900cdb91f70424fa880a8baf
ShailMurtaza <shailmurtaza@gmail.com>
2025-05-19 00:26:57 +0500

    Made model more complex to increase accuracy

--- test_model.py
+++ test_model.py
@@ -3,19 +3,16 @@ import cv2
 import numpy as np
 
 model = tf.keras.models.load_model("handwritten.keras")
-print("Modal loaded")
 
-# mnist = tf.keras.datasets.mnist
-# (x_train, y_train), (x_test, y_test) = mnist.load_data()
-# 
-# loss, accuracy = model.evaluate(x_test, y_test)
-# print(loss, accuracy)
-# 
-# input("ENTER")
-img = cv2.imread("2.bmp")[:,:,0]
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+loss, accuracy = model.evaluate(x_test, y_test)
+print(loss, accuracy)
+ 
+img = cv2.imread("9.1.bmp")[:,:,0]
 img = np.array([img])
-print("Image Loaded")
 
 pr = model.predict(img)
-print(np.argmax(pr))
+print(pr.argmax())
 
--- train_model.py
+++ train_model.py
@@ -1,4 +1,3 @@
-import os
 import tensorflow as tf
 
 # os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
@@ -14,19 +13,26 @@ x_test = x_test / 255.0
 
 
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(28, 28, 1)),
+    tf.keras.layers.Conv2D(32, (5, 5), activation='relu', padding='same', input_shape=(28, 28, 1)),
+    tf.keras.layers.Conv2D(32, (5, 5), activation='relu', padding='same'),
     tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Dropout(0.25),
 
     tf.keras.layers.Conv2D(64, (3, 3), activation='relu', padding='same'),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu', padding='same'),
     tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Dropout(0.25),
 
     tf.keras.layers.Conv2D(128, (3, 3), activation='relu', padding='same'),
+    tf.keras.layers.Conv2D(128, (3, 3), activation='relu', padding='same'),
     tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Dropout(0.25),
 
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(256, activation='relu'),
-    tf.keras.layers.Dropout(0.3),
+    tf.keras.layers.Dropout(0.5),
     tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.5),
     tf.keras.layers.Dense(10, activation='softmax')
 ])
 
