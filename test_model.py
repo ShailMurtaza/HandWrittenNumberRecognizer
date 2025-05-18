793661591a28842b0bbc9ca5e5c907207794f3b2
ShailMurtaza <shailmurtaza@gmail.com>
2025-05-18 14:16:40 +0500

    Model: Trained model again with more layers and more perceptrons each layer to capture more details. Also added 1 more convolutional layer

--- test_model.py
+++ test_model.py
@@ -3,16 +3,18 @@ import cv2
 import numpy as np
 
 model = tf.keras.models.load_model("handwritten.keras")
+print("Modal loaded")
 
-mnist = tf.keras.datasets.mnist
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-loss, accuracy = model.evaluate(x_test, y_test)
-print(loss, accuracy)
+# mnist = tf.keras.datasets.mnist
+# (x_train, y_train), (x_test, y_test) = mnist.load_data()
+# 
+# loss, accuracy = model.evaluate(x_test, y_test)
+# print(loss, accuracy)
 # 
 # input("ENTER")
-img = cv2.imread("2.webp")[:,:,0]
+img = cv2.imread("2.bmp")[:,:,0]
 img = np.array([img])
+print("Image Loaded")
 
 pr = model.predict(img)
 print(np.argmax(pr))
--- train_model.py
+++ train_model.py
@@ -5,22 +5,32 @@ import tensorflow as tf
 mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-x_train = tf.keras.utils.normalize(x_train, axis=1)
-x_test = tf.keras.utils.normalize(x_test, axis=1)
+x_train = x_train.reshape(-1, 28, 28, 1)
+x_test = x_test.reshape(-1, 28, 28, 1)
+
+# Normalize after reshaping (axis=1 is not suitable for images)
+x_train = x_train / 255.0
+x_test = x_test / 255.0
+
 
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
-    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(28, 28, 1)),
+    tf.keras.layers.MaxPooling2D(2, 2),
 
-    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
-    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu', padding='same'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+
+    tf.keras.layers.Conv2D(128, (3, 3), activation='relu', padding='same'),
+    tf.keras.layers.MaxPooling2D(2, 2),
 
     tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(256, activation='relu'),
+    tf.keras.layers.Dropout(0.3),
     tf.keras.layers.Dense(128, activation='relu'),
     tf.keras.layers.Dense(10, activation='softmax')
 ])
 
 model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
 
-model.fit(x_train, y_train, epochs=4)
+model.fit(x_train, y_train, epochs=15)
 model.save("handwritten.keras")
