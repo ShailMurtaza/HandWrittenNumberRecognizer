f2763afeba6e223e979b1a2d04a9a2485e4a3e88
ShailMurtaza <shailmurtaza@gmail.com>
2025-06-23 22:50:37 +0500

    Incorrect testing. Model input data needed to be noramlized

--- Model/test_model.py
+++ Model/test_model.py
@@ -6,13 +6,10 @@ model = tf.keras.models.load_model("handwritten.keras")
 
 mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_test = x_test.astype("float32") / 255.0
+x_test = np.expand_dims(x_test, axis=-1)
 
 loss, accuracy = model.evaluate(x_test, y_test)
-print(loss, accuracy)
+print("Loss:", loss)
+print("Accuracy:", accuracy)
  
-img = cv2.imread("9.1.bmp")[:,:,0]
-img = np.array([img])
-
-pr = model.predict(img)
-print(pr.argmax())
-
